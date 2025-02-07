af21ab7b5a3bebaecd83d9036603ccd4dbce003f
micheleboanno92 <michelebonanno92@gmail.com>
2025-02-07 12:33:47 +0100

    ripulito template

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return { 
-      message: 'Template Vite + Vue'
+     
     }
   }
 }
@@ -10,22 +10,12 @@ export default {
 
 <template>
   <header>
-    <h1>
-      {{ message }}
-    </h1>
+  
   </header>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-header {
-  background-color: $mainBgColor;
-  text-align: center;
-  padding: 20px 0;
-}
 
-h1 {
-  color: $mainColor;
-}
 </style>
