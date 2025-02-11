00725170c4a88bfebf8f72b11815881af90d3f89
micheleboanno92 <michelebonanno92@gmail.com>
2025-02-11 19:49:29 +0100

    fix header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,9 +19,16 @@ export default {
     <div>
       <button class="button-header">Cerca</button>
     </div>
-    <div>
-      <i class="fa-regular fa-heart"></i>
-
+    <div class="header-right">
+      <a href="#">
+        <i class="fa-regular fa-heart"></i>
+      </a>
+      <a href="#">
+        <i class="fa-solid fa-house"></i>
+      </a>
+      <a href="#" class="profile-circle">
+        <img src="/public/profile.jpg" alt="Foto profilo">
+      </a>
     </div>
 
   </header>
@@ -34,7 +41,7 @@ header{
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px;
+  padding: 20px;
 }
 header img{
   max-width: 100px;
@@ -50,5 +57,48 @@ header img{
   border-color: #b0b0b0;
   color: black;  
 }
+
+.fa-heart {
+  font-size: 24px;  /* Dimensione */
+  color: black;     /* Colore del bordo */
+  cursor: pointer;
+}
+.fa-house {
+  font-size: 24px;  /* Dimensione */
+  color: black;     /* Colore nero */
+}
+
+.profile-circle {
+  width: 50px;                /* Larghezza del cerchio */
+  height: 50px;               /* Altezza del cerchio */
+  border-radius: 50%;          /* Trasforma in cerchio */
+  overflow: hidden;            /* Taglia i contenuti in eccesso */
+  display: flex;               /* Allinea il contenuto (immagine) */
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(45deg, #f09433, #e6683c, #dc2743, #cc2366, #bc1888); /* Gradiente Instagram */
+  padding: 2px;                /* Spazio interno per il bordo gradiente */
+}
+
+.profile-circle img {
+  width: 100%;                 /* L'immagine riempie il cerchio */
+  height: 100%;                /* Mantiene proporzioni */
+  object-fit: cover;           /* Taglia l'immagine per adattarla al cerchio */
+  border-radius: 50%;          /* Arrotonda l'immagine per seguire il cerchio */
+}
+
+.header-right{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-right > a{
+  margin-right: 20px;
+}
+
+
+
+
 
 </style>
