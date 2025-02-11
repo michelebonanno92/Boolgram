85418f3ca21a58db7732c7b9d8fcaca85864dd91
micheleboanno92 <michelebonanno92@gmail.com>
2025-02-11 19:22:11 +0100

    stilizzato header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,12 +10,40 @@ export default {
 
 <template>
   <header>
-  
+    <div>
+      <img src="/public/logo.png" alt="logo">
+    </div>
+    <div>
+      <button class="button-header">Cerca</button>
+    </div>
+    <div>
+
+    </div>
+
   </header>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header img{
+  max-width: 100px;
+}
+.button-header{
+  padding: 0 80px;
+  background-color: white;
+  border: 2px solid #d3d3d3;
+  color: gray;
+  // transition: border-color 0.3s ease;
+}
+.button-header:hover{
+  border-color: #b0b0b0;
+  color: black;  
+}
 
 </style>
